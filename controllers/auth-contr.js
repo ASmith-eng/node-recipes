197a016c6e8b94b21fe78a044175db6fd8624fc6
ASmith-eng <alexsmith194@outlook.com>
2023-05-11 12:11:30 +0100

    Add logout functionality to delete session cookie

--- controllers/auth-contr.js
+++ controllers/auth-contr.js
@@ -1,8 +1,22 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
     res.render('./auth/login');
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isAuthenticated = true;
-    res.redirect('/');
+    User.queryUserById('645a89cc5238b8f44235c293')
+        .then(user => {
+            req.session.isAuthenticated = true;
+            req.session.user = user;
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
