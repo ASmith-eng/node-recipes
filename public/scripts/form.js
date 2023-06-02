cf64417efb4594b82257c248c1f99828e43b45f8
ASmith-eng <alexsmith194@outlook.com>
2023-06-02 23:46:29 +0100

    Add edit-ingredients + styling, frontend js

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,8 +1,10 @@
 let container = document.getElementById('field-container');
-let index = 1
+let index = container.dataset.ingtotal;
 
 document.getElementById('add-field').onclick = function () {
+    const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
     let template = `
+        <p>Ingredient ${+index+1}</p>
         <div class="name">
             <label for="name">Name</label>
             <input id="ingname-input" type="text" name="ingredient[${index}]name">
@@ -21,5 +23,6 @@ document.getElementById('add-field').onclick = function () {
     newIngredient.setAttribute('class', 'ingredient');
 
     container.appendChild(newIngredient);
+    window.scrollTo(0, scrollPos);
     index++;
 }
